86344ade0cf5d8b3f38ffd428364aacbd43e9971
Gabriel Monroy <monroygabriel9@gmail.com>
2024-05-05 18:25:53 -0600

    solo se añadio aspectos a agregar en Auditorios y Reservacion

--- src/pages/Auditorios.jsx
+++ src/pages/Auditorios.jsx
@@ -4,6 +4,7 @@ export const Auditorios = () => {
       <div className="md:px-12 p-4 max-w-screen-2xl mx-auto mt-24">
         <div className="bg-white shadow-md rounded-lg overflow-hidden w-1/5">
           {/* <img class="w-full h-48 object-cover" src="image.jpg" alt="Card image" /> */}
+          <h1>Aqui es donde iran los auditorios</h1>
           <div className="p-4">
             <h5 className="text-xl font-semibold text-gray-900">Card Title</h5>
             <p className="text-gray-700 mb-3">This is some example text for the card description.</p>
--- src/pages/Reservacion.jsx
+++ src/pages/Reservacion.jsx
@@ -1,7 +1,11 @@
 export const Reservacion = () => {
   return (
     <>
-      <h1>Reservacion</h1>
+      <div className="md:px-12 p-4 max-w-screen-2xl mx-auto mt-24">
+
+        <h1>Aqui se inserta el formulario de reservacion</h1>
+
+      </div>
     </>
   );
 };
